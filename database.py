d7ab2f1ee0b46ccea3ae94fb77146e798ecaedac
Jeffrin Joel Raj <joeljeffrin@gmail.com>
2026-06-19 00:35:07 +0530

    Add Docker support and env var configuration

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://jeffrin:@localhost/bookreview"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://jeffrin:@localhost/bookreview")
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta, timezone
+import os
+from dotenv import load_dotenv
 
-SECRET_KEY = "your_key"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY", "your_key")
 ALGORITHM = "HS256"
+
 
 def create_token(data: dict):
     to_encode = data.copy()
